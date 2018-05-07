a4773f595fb1c5430690762e95770c57f81118f6
Lora9876 <mina.divida@gmail.com>
2018-05-07 17:22:40 +0300

    Update hello.cu

--- hello.cu
+++ hello.cu
@@ -21,7 +21,7 @@ int main()
 	printf("%f\n", b1);
 	printf("%f\n", theta1);
 	
- 
+ 	read_the_files(); 
 	/*char *ad;
 	int *bd;
 	const char csize = N*sizeof(char);
@@ -45,8 +45,9 @@ int main()
 	return EXIT_SUCCESS;
 }
 
-void read_the_files(FILE *real_g, FILE *synthetic_g)
+void read_the_files()
 {
+	FILE *real_g, FILE *synthetic_g;
 	int galaxies_r, galaxies_s; 
 	float *a0, *a1, *b0, *b1;
 	real_g = fopen("data_100k_arcmin.txt","r");
