fa54600c3d84f8da7c77c63735a6e0cc620371fa
Lora9876 <mina.divida@gmail.com>
2018-05-13 23:29:57 +0200

    Update hello.cu

--- hello.cu
+++ hello.cu
@@ -148,7 +148,7 @@ __global__ void angles(volatile float *a0, volatile float *b0, volatile float *a
   
     memset(hist_array,0,hist_array_size); 
 	 cudaMemset(tmp,0,size_h_bytes);
-	   angles<<<grid,block>>>(aa0, bb0,aa1, bb1, 0, 0, 512, 512, tmp);
+	   angles<<<grid,block>>>(a0, b0,a1, b1, 0, 0, 512, 512, tmp);
             cudaMemcpy(hist, tmp, size_h_bytes, cudaMemcpyDeviceToHost);
 	 
 	 for(int i=0; i<720; i++)
