0e0f318175d27548e9415d86923d6fa209c323b4
Lora9876 <mina.divida@gmail.com>
2018-05-14 22:25:54 +0200

    Update hellowww.cu

--- hellowww.cu
+++ hellowww.cu
@@ -23,9 +23,12 @@ __global__ void angles(volatile float *a0, volatile float *b1, volatile float *h
    	 __syncthreads();
 
   if(idx<20 && idy<20)
-             {int shared[idx]= a0[idx] + b1[idy]+ shared[idx];}
-   
-             }
+	float m=  a0[idx] + b1[idy];
+             
+    __syncthreads();
+	shared[idx]+=m; 
+}
+             
              
   int main()
              {
