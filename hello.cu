4a6281bd15fb4ca379ccd66ab5989dca674e37fa
Lora9876 <mina.divida@gmail.com>
2018-05-14 14:39:26 +0300

    Update hello.cu

--- hello.cu
+++ hello.cu
@@ -18,7 +18,7 @@ __global__ void angles(volatile float *a0, volatile float *b0, volatile float *a
 {
 	int idx = blockIdx.x * blockDim.x + threadIdx.x;// ovo proveri
 	float angle; 
-   	int idy = blockIdx.y * blockDim.y + threadIdx.y;
+   	
 	
 	
  __shared__ unsigned int shared[bins];
@@ -34,14 +34,14 @@ __global__ void angles(volatile float *a0, volatile float *b0, volatile float *a
     {
        
       //provera
-
+			for(int i=0; i<ymax; i++)
        	
-                angle = acos(sin(b0[idx]) *sin(b1[idy]) + cos(b0[idy]) * cos(b1[idy]) * cos(a0[idx]-a1[idy]))*val;
+			{ angle = acos(sin(b0[idx]) *sin(b1[i]) + cos(b0[i]) * cos(b1[i]) * cos(a0[idx]-a1[i]))*val;
 		
 		shared[int(angle/0.25)]++ ;
 		//nadji nacin da atomic add proradi :D
 		//atomicAdd(&shared[int(angle)],1); 
-	
+			}
     __syncthreads();
 
     if(threadIdx.x==0& threadIdx.y==0)
