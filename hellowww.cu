1894ce3dc310e93826fe536b8f89b37cbcfb9e4c
Lora9876 <mina.divida@gmail.com>
2018-05-15 23:22:18 +0200

    Update hellowww.cu

--- hellowww.cu
+++ hellowww.cu
@@ -20,7 +20,10 @@ __global__ void VecAdd(float* A, float* B, float* C, int N)
  		float *addr; 
 		int idx = blockDim.x * blockIdx.x + threadIdx.x;
 			
-	
+		__shared__ float mn[720];
+ 			if(threadIdx.x==0)
+				for(int i=0; i<720; i++)
+					mn[i]=0; 
     __syncthreads();
 	
 			if (idx<10000)
@@ -28,11 +31,17 @@ __global__ void VecAdd(float* A, float* B, float* C, int N)
 				{
 					m=A[idx]*B[i];
 					n=int(m); 
-					C[n]++ ;
+					mn[n]++ ;
 					 
 				}
  							
-	
+	__syncthreads();
+
+    if(threadIdx.x==0)
+    {
+        for(int i=0;i<720;i++)
+            C[i+(blockIdx.x*720)]=mn[i];
+    }
  
 
 }
