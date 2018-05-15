75b7b7da43a9e5ee9edd947c4d896e37d81f3af9
Lora9876 <mina.divida@gmail.com>
2018-05-15 17:58:16 +0200

    Update hellowww.cu

--- hellowww.cu
+++ hellowww.cu
@@ -31,16 +31,13 @@ int main(int argc, char *argv[])
     cudaGetDevice(&device);
     cudaGetDeviceProperties(&prop, device);
     
-    cudaOccupancyMaxActiveBlocksPerMultiprocessor(
-        &numBlocks,
-        MyKernel,
-        blockSize,
-        0);
+    
 
-    activeWarps = numBlocks * blockSize / prop.warpSize;
-    maxWarps = prop.maxThreadsPerMultiProcessor / prop.warpSize;
+   // activeWarps = numBlocks * blockSize / prop.warpSize;
+   // maxWarps = prop.maxThreadsPerMultiProcessor / prop.warpSize;
 
-    std::cout << "Occupancy: " << (double)activeWarps / maxWarps * 100 << "%" << std::endl;
+   printf("%d", prop.warpSize); 
+	printf("%d", prop.maxThreadsPerMultiProcessor); 
     
 
 int N =50;
