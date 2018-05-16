3f0613d4c2e5f960380d5e0d0e022d1a8af391c0
Lora9876 <mina.divida@gmail.com>
2018-05-16 19:42:11 +0200

    Update hellowww.cu

--- hellowww.cu
+++ hellowww.cu
@@ -73,8 +73,8 @@ cudaMemcpy(d_A, h_A, arraybytes, cudaMemcpyHostToDevice);
 cudaMemcpy(d_B, h_B, arraybytes, cudaMemcpyHostToDevice);
 // Invoke kernel
 
-	dim3 thr(512,512);
-	int blocksInGrid=512; 
+	int th3=1024
+	dim3 blocksInGrid(1024,1024); 
 	clock_t start, end;
      double cpu_time_used;
      
