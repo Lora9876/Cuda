329ce215d7181c4afdac3703ef4fb20a37535219
Lora9876 <mina.divida@gmail.com>
2018-05-16 23:35:11 +0200

    Update hellowww.cu

--- hellowww.cu
+++ hellowww.cu
@@ -32,7 +32,7 @@ __global__ void VecAdd(float* A, float* B, int* C, int N,int sum)
 			{	for(int i=0;i<100; i++)
 			{		m=A[100*idx+i]*B[i*100+idy];
 					n=int(m); 
-					mn[0]++}}
+					mn[0]++;}}
  
  			
 /*if (idx<10000)
