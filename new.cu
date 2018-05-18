9d234e00d4c72454b9327e714908e0e6e4898b99
Lora9876 <mina.divida@gmail.com>
2018-05-18 23:12:36 +0200

    Update new.cu

--- new.cu
+++ new.cu
@@ -82,8 +82,8 @@ cudaMemcpy(d_B, h_B, arraybytes, cudaMemcpyHostToDevice);
 // Invoke kernel
 	
 	clock_t start, end;
-    int threadsPerBlock=704;
-    int blocksPerGrid=60; 
+    int threadsPerBlock=736;
+    int blocksPerGrid=14; 
 	/* dim3 threadsPerBlock(128, 128);
     dim3 blocksPerGrid(1, 1);
         /*if (NN*NN > 512){
