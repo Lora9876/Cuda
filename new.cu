b55767b392b8bb85816ae9564748fa25e1495c41
Lora9876 <mina.divida@gmail.com>
2018-05-18 22:48:30 +0200

    Update new.cu

--- new.cu
+++ new.cu
@@ -13,7 +13,7 @@ using namespace std;
 
 __global__ void angles(volatile float *a0, volatile float *b0,   volatile int *hist)
 
-    
+{
     int idx = blockIdx.x * blockDim.x + threadIdx.x; // This should range to SUBMATRIX_SIZE
 
    
