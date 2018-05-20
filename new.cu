6c30cb1e3c30ee4c3e1c288efae41a448f65f85e
Lora9876 <mina.divida@gmail.com>
2018-05-20 20:47:41 +0200

    Update new.cu

--- new.cu
+++ new.cu
@@ -66,7 +66,7 @@ __global__ void angles(volatile float *a0, volatile float *b0, volatile float *a
 
     __syncthreads();
 
-    if(threadIdx.x==0)
+    if(threadIdx.x==0 && threadIdx.y==0)
     {
         for(int i=0;i<720;i++)
 	{ hist[i+(blockIdx.x*720)]=mn[i]; hist_r[i+(blockIdx.x*720)]=r[i]; hist_s[i+(blockIdx.x*720)]=s[i];}
