7432899ac85f6c088acc643df072f45d49a7dcad
Lora9876 <mina.divida@gmail.com>
2018-05-20 21:12:26 +0200

    Update new.cu

--- new.cu
+++ new.cu
@@ -40,7 +40,9 @@ __global__ void angles(volatile float *a0, volatile float *b0, volatile float *a
         	{
             ac= acosf((sin(b0[idx]*fix1)*sin(b1[i]*fix1))+ cos(b0[idx]*fix1)*cos(b1[i]*fix1)*cos((a1[i]-a0[idx])*fix1));
 		ac= (ac*fix2/0.25); 
+	
 		angle=(int) ac; 
+		if(angle>719) angle=0; 
              atomicAdd(&mn[angle],1);
 		}
 	  /*  for(int i=idx+1; i<100000;i++)
