d3735d563d586708df0c9a5d8a87c15b08966bb5
Lora9876 <mina.divida@gmail.com>
2018-05-31 11:24:02 +0300

    Update new.cu

--- new.cu
+++ new.cu
@@ -53,7 +53,7 @@ __global__ void angles(volatile float *a0, volatile float *b0, volatile float *a
 			ccb1=ccb1+minus*pom/factorial; 
 			sb1=sb1+minus*k*pom/factorials; 
 			factorial=factorial*fb*(fb+1); fb+=2;
-			factorials=factorials*fsb*(fsb+1); 
+			factorials=factorials*fsb*(fsb+1);  fsb+=2; 
 		}
 		 /* sb1=k-k*k*k/6 + k*k*k*k*k/120- k*k*k*k*k*k*k/5040+k*k*k*k*k*k*k*k*k/362880-k*k*k*k*k*k*k*k*k*k*k/39916800;
 		  ccb1=1-k*k/2+k*k*k*k/24-k*k*k*k*k*k/720+k*k*k*k*k*k*k*k/40320-k*k*k*k*k*k*k*k*k*k/3628800;*/
