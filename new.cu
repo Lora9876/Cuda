ea04ba43526a5cd7bc13c761023486955891f1f0
Lora9876 <mina.divida@gmail.com>
2018-06-01 12:00:04 +0300

    Update new.cu

--- new.cu
+++ new.cu
@@ -22,7 +22,7 @@ __global__ void angles(volatile float *a0, volatile float *b0, volatile float *a
 	
 	float ac, bb0,sb1,sb0,cb0,k,bb1,ssb1,cb1,ccb1,pom,minus,factorial, factorials,fb,fsb ,k1, pom1,ccd; 
     int angle;  float fix2=57;
-    	 k=b0[idx];  k1=b1[idx]; ssb1=sin(bb1); 
+    	bb0=b0[idx];  bb1=b1[idx]; ssb1=sin(bb1); 
      		sb0=sin(bb0); cb0=cos(bb0); cb1=cos(bb1);
 		
     __shared__ int mn[720], r[720], s[720];
